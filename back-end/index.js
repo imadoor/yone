577490a8522ecb67d6246c931f248cf41bc489fb
Amador Ollarzabal <amador.ollarzabal@outlook.com>
2020-09-11 23:59:28 -0500

    added the postgres module. added all the crud opperations to the express routes

--- back-end/index.js
+++ back-end/index.js
@@ -1,37 +1,130 @@
 require('dotenv').config();
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 const PORT = process.env.PORT;
 
+const db = require('./db');
 
-app.use((req, res, next) =>{
-    console.log("In middleware");
-    next();
-});
+app.use(morgan('dev'));
+app.use(express.json());
+// app.use((req, res, next) =>{
+//     console.log("In middleware");
+//     next();
+// });
 
 //Get all restaurants
-app.get('/api/v1/restaurants', (req, res) => {
-     res.status(200).json({
-         "status":"sucess",
-         "data": {
-             "restaurant":[
-                 "Chipotle",
-                 "Dominos"
-             ]
-         }
-     });
+app.get('/api/v1/restaurants', async (req, res) => {
+
+    try{
+        const {rows} = await db.query('SELECT * FROM restaurant');
+        console.log(rows);
+         res.status(200).json({
+             status:"sucess",
+             results: rows.length,
+             data: {
+                 restaurant: rows
+             }
+         });
+    } 
+    catch (err){
+        res.status(404).json({status: 'failed'});
+    }
+
 });
 
-app.get('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params);
+//Get specific restaurant
+app.get('/api/v1/restaurants/:id', async (req, res) => {
+
+    try{
+        const {rows} = await db.query('SELECT * FROM restaurant WHERE id = $1', [req.params.id]);
+        console.log(req.params);
+         res.status(200).json({
+             status:"sucess",
+             results: rows.length,
+             data: {
+                 restaurant: rows
+             }
+         });
+    } 
+    catch (err){
+        res.status(404).json({status: 'restaurant does not exist'});
+    }
 });
 
-app.post('/api/v1/restaurants/', (req, res) => {
-    console.log(req);
+//create restaurant entry
+app.post('/api/v1/restaurants/', async (req, res) => {
+    try{
+        console.log(req.body);
+        const { rows } = await db.query(
+            'INSERT INTO restaurant (name, location, price_range) VALUES ($1, $2, $3) returning *', 
+            [req.body.name, req.body.location, req.body.price_range]);
+         res.status(201).json({
+             status:"sucess",
+             results: rows.length,
+             data: {
+                 restaurant: rows
+             }
+         });
+    } 
+    catch (err){
+        res.status(404).json({status: 'restaurant does not exist'});
+    }
 });
 
 
+//update restaurant
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+    try{
+        console.log(req.body);
+        const {rowCount, rows} = await db.query(
+            'UPDATE restaurant SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *', 
+            [req.body.name, req.body.location, req.body.price_range, req.params.id]);
+        if(rowCount === 1){
+            res.status(201).json({
+                status:"sucess",
+                results: rows.length,
+                data: {
+                    restaurant: rows
+                }
+            });
+        }
+        else{
+            res.status(404).json({
+                status:"restaurant does not exist",
+            });
+        }
+    } 
+    catch (err){
+        res.status(404).json({status: 'restaurant does not exist'});
+    }
+});
+
+//delete restaurant
+app.delete('/api/v1/restaurants/:id', async (req, res) => {
+    try{
+        console.log(req.body);
+        const { rowCount, rows } = await db.query(
+            'DELETE FROM restaurant WHERE id=$1', [req.params.id]);
+        if(rowCount === 1){
+            res.status(201).json({
+                status:"sucess",
+                results: rows.length,
+                data: {
+                    restaurant: rows
+                }
+            });
+        }
+        else{
+            res.status(404).json({
+                status:"restaurant does not exist",
+            });
+        }
+        } 
+        catch (err){
+            res.status(404).json({status: 'restaurant does not exist'});
+        }
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
